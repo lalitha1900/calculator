081823e74e2d95dc7c5248118b01a1d92edd1766
lalitha1900 <trlalitha2000@gmail.com>
2024-03-26 02:06:45 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useEffect } from "react";
 
-function App() {
+export default function App() {
+  function calc(value) {
+    if (value === "C") {
+      document.getElementById("input").value = "";
+    } else if (value == "=") {
+      document.getElementById("output").innerHTML = eval(
+        document.getElementById("input").value
+      );
+    } else {
+      document.getElementById("input").value += value;
+    }
+    //document.getElementById("input").value += value;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h1>React Calculator</h1>
+
+      <input type="text" id="input" />
+      <br />
+      <br />
+      <div id="output"></div>
+      <br />
+      <div id="buttons">
+        <button name="7" onClick={(e) => calc(e.target.name)}>
+          7
+        </button>
+        <button name="8" onClick={(e) => calc(e.target.name)}>
+          8
+        </button>
+        <button name="9" onClick={(e) => calc(e.target.name)}>
+          9
+        </button>
+        <button name="+" onClick={(e) => calc(e.target.name)}>
+          +
+        </button>
+        <br />
+        <button name="4" onClick={(e) => calc(e.target.name)}>
+          4
+        </button>
+        <button name="5" onClick={(e) => calc(e.target.name)}>
+          5
+        </button>
+        <button name="6" onClick={(e) => calc(e.target.name)}>
+          6
+        </button>
+        <button name="-" onClick={(e) => calc(e.target.name)}>
+          -
+        </button>
+        <br />
+        <button name="1" onClick={(e) => calc(e.target.name)}>
+          1
+        </button>
+        <button name="2" onClick={(e) => calc(e.target.name)}>
+          2
+        </button>
+        <button name="3" onClick={(e) => calc(e.target.name)}>
+          3
+        </button>
+
+        <button name="*" onClick={(e) => calc(e.target.name)}>
+          *
+        </button>
+        <br />
+
+        <button name="C" onClick={(e) => calc(e.target.name)}>
+          C
+        </button>
+
+        <button name="0" onClick={(e) => calc(e.target.name)}>
+          0
+        </button>
+        <button name="=" onClick={(e) => calc(e.target.name)}>
+          =
+        </button>
+        <button name="/" onClick={(e) => calc(e.target.name)}>
+          /
+        </button>
+        <br />
+      </div>
+      <br />
+      <br />
     </div>
   );
 }
-
-export default App;
